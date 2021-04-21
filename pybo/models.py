ef36361255a0f4c126341bdf55c5cd767dc6da7f
Jade <jk042386@gmail.com>
2021-04-22 00:23:20 +0900

    Add Model(Question, Answer), Admin in pybo

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    subject = models.CharField(max_length=200) #최대 200자
+    content = models.TextField() #글자 수 제한 X
+    create_date = models.DateTimeField() #날짜, 시간 관련
+
+    def __str__(self):
+        return self.subject
+
+class Answer(models.Model):
+    # CASCADE = 답변에 연결된 질문이 삭제되면 답변도 함께 삭제
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
