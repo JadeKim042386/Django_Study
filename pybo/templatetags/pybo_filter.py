b4995627967cc39ef2f8f949659d6a047892edd2
Jade <jk042386@gmail.com>
2021-05-05 22:28:40 +0900

    Apply markdown

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,7 +1,18 @@
 from django import template
+import markdown
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.filter
 def sub(value, arg):
     return value - arg
+
+@register.filter()
+def mark(value):
+    '''
+    nl2br : 줄바꿈 문자를 <br> 태그로 바꿔 줌
+    fenced_code : 마크다운의 소스 코드 표현을 위해 적용
+    '''
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
